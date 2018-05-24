0663b561da75d549c5136fa16c65fa5f6430ccc2
Alan Golman <golman.alan@gmail.com>
2018-05-24 17:11:55 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,8 +4,8 @@ def my_select(collection)
   while i < array.length 
     if yield (collection[i])
       select << (collection[i])
-      i += 1 
-    end   
+    end 
+    i += 1
   end 
   select 
 end
